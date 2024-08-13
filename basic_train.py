d0b10772eefa9bc49f868336ee5b0ea8e5c04e56
Daniel Sh. <dshamsoddini@gmail.com>
2024-08-13 12:32:17 -0700

    improvement

--- basic_train.py
+++ basic_train.py
@@ -7,31 +7,51 @@ import evaluate
 import numpy as np
 
 
-tokenizer = AutoTokenizer.from_pretrained("google-t5/t5-base")
-model = AutoModelForCausalLM.from_pretrained("google-t5/t5-base")
+# tokenizer = AutoTokenizer.from_pretrained("google-t5/t5-base")
+# model = AutoModelForCausalLM.from_pretrained("google-t5/t5-base")
 
 df = pd.read_parquet("hf://datasets/kchawla123/casino/data/train-00000-of-00001.parquet")
 # print(df.head())
 
-training_args = TrainingArguments(output_dir="test_trainer", eval_strategy="epoch")
-metric = evaluate.load("accuracy")
-def compute_metrics(eval_pred):
-    logits, labels = eval_pred
-    predictions = np.argmax(logits, axis=-1)
-    return metric.compute(predictions=predictions, references=labels)
+# training_args = TrainingArguments(output_dir="test_trainer", eval_strategy="epoch")
+# metric = evaluate.load("accuracy")
+# def compute_metrics(eval_pred):
+#     logits, labels = eval_pred
+#     predictions = np.argmax(logits, axis=-1)
+#     return metric.compute(predictions=predictions, references=labels)
 
-
+CONTEXT_SIZE = 5
+other_id = {"mturk_agent_1":"mturk_agent_2", "mturk_agent_2":"mturk_agent_1"}
 dialogs = []
+results = []
 for _,a in df.iterrows():
     dialog = []
     priorities = [a['participant_info']['mturk_agent_1']["value2issue"], a['participant_info']['mturk_agent_2']["value2issue"]]
-    dialog.append(priorities)
+    final_offer = {}
     for b in a['chat_logs']:
         # print(b)
-        dialog.append({b['id']:b["text"]})
+        
+        if b['text'] == "Submit-Deal":
+            # print(b)
+            # final_offer[b['id']] = b['task_data']['issue2youget']
+            # final_offer[other_id[b['id']]] = b['task_data']['issue2theyget']
+            line_text =f"{b["text"]} {b['task_data']['issue2youget']['Firewood']} Firewood {b['task_data']['issue2youget']['Water']} Water {b['task_data']['issue2youget']['Food']} Food"
+            dialog.append({b['id']:line_text})
+        else:
+            dialog.append({b['id']:b["text"]})
+    result = []
+    arr = dialog
+    for i in range(len(arr)):
+        if i < CONTEXT_SIZE:
+            result.append(arr[:i])
+        else:
+            result.append(arr[i-CONTEXT_SIZE:i])
+    
+    results.append(result)
     dialogs.append(dialog)
 
 print(dialogs[0])   
+# print(results[0])
 # print(dialogs)
 # with open("data.txt", "w") as f:
 #     f.write(str(dialogs[0]))
