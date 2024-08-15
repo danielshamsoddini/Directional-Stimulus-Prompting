f8b089d8a15486a27fa6d6f846363e4ddd94ebc0
Daniel Sh. <dshamsoddini@gmail.com>
2024-08-15 12:36:41 -0700

    updates and now doesnt constantly repeat same phrase

---
+++ sv+ppo/dialog.py
@@ -0,0 +1,67 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM, AutoModelForSeq2SeqLM
+import pandas as pd
+from transformers import TrainingArguments, Trainer
+import evaluate
+import numpy as np
+import datasets
+from transformers import DataCollatorForSeq2Seq
+import random
+generation_params = {
+    "max_length": 600,
+    "no_repeat_ngram_size": 1,
+    "do_sample": True,
+    "top_k": 50,
+    "top_p": 0.95,
+    "temperature": 0.7,
+    "num_return_sequences": 1,
+    "repetition_penalty": 1.3,
+}
+class SupervisedAgent:
+    def __init__(self, id, model_dir):
+        self.model = AutoModelForSeq2SeqLM.from_pretrained(model_dir,local_files_only=True)
+        self.tokenizer = AutoTokenizer.from_pretrained(model_dir,local_files_only=True)
+        self.priorities = "Priorities: Low Firewood Medium Water High Food "
+        self.id = id
+
+    def respond(self, text):
+        print(text)
+        inputs = self.tokenizer(["Continue writing the following text.\n\n"+ self.priorities + text], return_tensors="pt")
+        reply_ids = self.model.generate(**inputs, **generation_params)
+        return self.tokenizer.decode(reply_ids[0], skip_special_tokens=True)
+    
+class Dialog:
+    def __init__(self, agents):
+        self.agents = agents
+        self.dialog_history = []
+        self.num_rounds = 10
+
+    def selfplay(self):
+        random.shuffle(self.agents)
+        flag = False
+        for a in range(self.num_rounds):
+            for agent in self.agents:
+                prev_convo = self.dialog_history[-4:]
+                convo_str = ""
+                if len(prev_convo) > 0:
+                    for i in prev_convo:
+                        you_or_them = "YOU: " if list(i.keys())[0] == agent.id else "THEM: "
+                        convo_str += f"{you_or_them}: {list(i.values())[0]} "
+                convo_str += "YOU: "
+
+                
+                self.dialog_history.append({agent.id:agent.respond(convo_str)})
+                if "Accept-Deal" in list(self.dialog_history[-1].values())[0]:
+                    flag = True
+                    break
+            if flag:
+                break
+        
+        self.print_dialog()
+    
+    def print_dialog(self):
+        for line in self.dialog_history:
+            print(line)
+
+Dialog([SupervisedAgent("agent1","flan_t5-small-casino/checkpoint-14120"), SupervisedAgent("agent2","flan_t5-small-casino/checkpoint-14120")]).selfplay()
+
+        
